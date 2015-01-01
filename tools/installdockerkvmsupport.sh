63de967f98c4c435413e36fcf55c82542a11bd0b
kristof de spiegeleer <kristof@incubaid.com>
2015-01-01 10:03:12 +0200

    Update installdockerkvmsupport.sh

--- tools/installdockerkvmsupport.sh
+++ tools/installdockerkvmsupport.sh
@@ -10,16 +10,21 @@ jsconfig hrdset -n whoami.email -v ''
 jsconfig hrdset -n whoami.fullname -v ''
 jsconfig hrdset -n whoami.git.login -v ''
 jsconfig hrdset -n whoami.git.passwd -v ''
+echo 'HRD SET' >> /opt/jumpscale7/done.txt
 
 jpackage install -n docker
+echo 'DOCKER DONE' >> /opt/jumpscale7/done.txt
 jpackage install -n kvm
+echo 'KVM DONE' >> /opt/jumpscale7/done.txt
 
 apt-get install linux-image-generic -f -y
+echo 'APT KERNEL DONE' >> /opt/jumpscale7/done.txt
 mkdir -p ~/ovh.d/
 sudo mv /etc/grub.d/06_OVHkernel ~/ovh.d
 sudo update-grub
+echo 'GRUB DONE' >> /opt/jumpscale7/done.txt
 sudo apt-get install linux-headers-generic -f -y
 
 echo INSTALL DONE
 
-echo 'INSTALL DONE' > /opt/jumpscale7/done.txt
+echo 'INSTALL DONE' >> /opt/jumpscale7/done.txt
