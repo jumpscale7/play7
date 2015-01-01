4df1af78a1c80a1495d76856f47fa5d630ddcf9d
kristof de spiegeleer <kristof@incubaid.com>
2015-01-01 10:41:05 +0200

    Update installdockerkvmsupport.sh

--- tools/installdockerkvmsupport.sh
+++ tools/installdockerkvmsupport.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get upgrade
-apt-get install mc curl git ssh python2.7 python-requests  -y
+apt-get install mc curl git ssh python2.7 python-requests resolvconf -y
 set -ex
 curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
 cd /tmp
