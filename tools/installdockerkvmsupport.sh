6343c1e1eb4410c94e17e6fc997dd73ba7eb37e9
kristof de spiegeleer <kristof@incubaid.com>
2015-04-28 12:44:28 +0400

    temp

--- tools/installdockerkvmsupport.sh
+++ tools/installdockerkvmsupport.sh
@@ -2,7 +2,8 @@ apt-get update
 apt-get upgrade
 apt-get install mc curl git ssh python2.7 python-requests resolvconf -y
 set -ex
-curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
+rm -f /tmp/bootstrap_web.py
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/@ys/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
 cd /tmp
 python /tmp/bootstrap_web.py
 
@@ -12,24 +13,24 @@ python /tmp/bootstrap_web.py
 #jsconfig hrdset -n whoami.git.login -v ''
 #jsconfig hrdset -n whoami.git.passwd -v ''
 
-echo 'email =' > /opt/jumpscale7/hrd/system/whoami.hrd
-echo 'fullname =' >> /opt/jumpscale7/hrd/system/whoami.hrd
-echo 'git.login =' >> /opt/jumpscale7/hrd/system/whoami.hrd
-echo 'git.passwd =' >> /opt/jumpscale7/hrd/system/whoami.hrd
+#echo 'email =' > /opt/jumpscale7/hrd/system/whoami.hrd
+#echo 'fullname =' >> /opt/jumpscale7/hrd/system/whoami.hrd
+#echo 'git.login =' >> /opt/jumpscale7/hrd/system/whoami.hrd
+#echo 'git.passwd =' >> /opt/jumpscale7/hrd/system/whoami.hrd
 
-echo 'HRD SET' >> /opt/jumpscale7/done.txt
+#echo 'HRD SET' >> /opt/jumpscale7/done.txt
 
-jpackage install -n docker
+#ays install -n docker
 echo 'DOCKER DONE' >> /opt/jumpscale7/done.txt
-jpackage install -n kvm
+#ays install -n kvm
 echo 'KVM DONE' >> /opt/jumpscale7/done.txt
 
-apt-get install linux-image-generic -f -y
-echo 'APT KERNEL DONE' >> /opt/jumpscale7/done.txt
-sudo rm -rf /etc/grub.d/06_OVHkernel
-sudo update-grub
-echo 'GRUB DONE' >> /opt/jumpscale7/done.txt
-sudo apt-get install linux-headers-generic -f -y
+#apt-get install linux-image-generic -f -y
+#echo 'APT KERNEL DONE' >> /opt/jumpscale7/done.txt
+#sudo rm -rf /etc/grub.d/06_OVHkernel
+#sudo update-grub
+#echo 'GRUB DONE' >> /opt/jumpscale7/done.txt
+#sudo apt-get install linux-headers-generic -f -y
 
 echo INSTALL DONE
 
