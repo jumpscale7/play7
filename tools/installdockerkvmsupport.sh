e1c853e9cd25cdcbf1e9d6b3c8b5f5a3e8514f5f
kristof de spiegeleer <kristof@incubaid.com>
2015-09-05 15:09:39 +0200

    Update installdockerkvmsupport.sh
    UPDATE

--- tools/installdockerkvmsupport.sh
+++ tools/installdockerkvmsupport.sh
@@ -2,29 +2,18 @@ apt-get update
 apt-get upgrade
 apt-get install mc curl git ssh python2.7 python-requests resolvconf -y
 set -ex
-rm -f /tmp/bootstrap_web.py
-curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/@ys/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
+rm -f /tmp/install.py
+curl https://github.com/Jumpscale/jumpscale_core7/blob/master/install/install.sh > /tmp/install.py
 cd /tmp
-python /tmp/bootstrap_web.py
+python /tmp/install.py
 
-##NEED TO FIX
-#jsconfig hrdset -n whoami.email -v ''
-#jsconfig hrdset -n whoami.fullname -v ''
-#jsconfig hrdset -n whoami.git.login -v ''
-#jsconfig hrdset -n whoami.git.passwd -v ''
-
-#echo 'email =' > /opt/jumpscale7/hrd/system/whoami.hrd
-#echo 'fullname =' >> /opt/jumpscale7/hrd/system/whoami.hrd
-#echo 'git.login =' >> /opt/jumpscale7/hrd/system/whoami.hrd
-#echo 'git.passwd =' >> /opt/jumpscale7/hrd/system/whoami.hrd
-
-#echo 'HRD SET' >> /opt/jumpscale7/done.txt
-
-#ays install -n docker
+ays install -n docker
 echo 'DOCKER DONE' >> /opt/jumpscale7/done.txt
-#ays install -n kvm
+
+ays install -n kvm
 echo 'KVM DONE' >> /opt/jumpscale7/done.txt
 
+#WHEN OVH
 #apt-get install linux-image-generic -f -y
 #echo 'APT KERNEL DONE' >> /opt/jumpscale7/done.txt
 #sudo rm -rf /etc/grub.d/06_OVHkernel
