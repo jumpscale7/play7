0b5d37c2bc0dae506fde054d3f1a99d018a8bd5b
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-09-07 14:10:19 +0200

    Rework do.py to use ays services

--- docker_jumpscale_development/do.py
+++ docker_jumpscale_development/do.py
@@ -1,48 +1,27 @@
 from JumpScale import j
 
 
-def _create_docker_machine(name, image='despiegk/mc'):
+def docker_create_machine(name, reinstall=False, image='despiegk/mc'):
+    docker = j.atyourservice.findTemplates(name='node.docker')[0]
     ports = "8086:8086 8083:8083 28017:28017 27017:27017 5544:5544 82:82"
     vols = "/opt/jumpscale/var/influxdb:/var/mydocker/influxdb # /opt/jumpscale/var/mongodb:/var/mydocker/mongodb"
-    j.tools.docker.destroy(name)
-    j.tools.docker.create(
-        name=name,
-        ports=ports,
-        vols=vols,
-        volsro='',
-        stdout=True,
-        base=image,
-        nameserver=['8.8.8.8'],
-        replace=True,
-        cpu=None,
-        mem=0,
-        jumpscale=True
-    )
+    args = {
+        'instance.param.name': name,
+        'instance.param.image': image,
+        'instance.param.portsforwards': ports,
+        'instance.param.volumes': vols,
+    }
 
-
-def docker_create_machine(name='master', reset=False, image='despiegk/mc'):
-    containers = j.tools.docker.list()
-    if name not in containers or reset:
-        _create_docker_machine(name, image)
-
-    return j.tools.docker.getSSH(name)
-
-
-def install_portal(ssh):
-    """
-    install portal
-    """
-
-    ssh.run('ays install -n singlenode_portal')
-    ssh.run('ays start')
-
+    docker.install(name, reinstall=reinstall, args=args)
 
 ###################################################################################
 if __name__ == '__main__':
     name = 'master'
-    ssh = docker_create_machine(name=name, reset=True)
+    docker_create_machine(name=name, reinstall=True)
+    instance = j.atyourservice.findServices(name='node.docker', instance=name)[0]
 
-    install_portal(ssh)
+    portal = j.atyourservice.findTemplates(name='singlenode_portal')[0]
+    portal.install(parent=instance)
 
     info = j.tools.docker.inspect(name)
 
