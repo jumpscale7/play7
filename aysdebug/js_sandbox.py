7c69ecd64a28dcc40dfd527e2f19f8d4360c1e59
kristof de spiegeleer <kristof@incubaid.com>
2015-12-11 05:54:25 +0400

    sss

--- aysdebug/js_sandbox.py
+++ aysdebug/js_sandbox.py
@@ -2,8 +2,17 @@ import sys
 from JumpScale import j
 
 j.do.createDir("/usr/local/lib/python3.5/site-packages")
-j.do.symlink("/opt/jumpscale7/lib/JumpScale/","/usr/local/lib/python3.5/site-packages/JumpScale/")
-j.do.symlink("/opt/jumpscale7/lib/JumpScale/","/root/.ipython/JumpScale/")
+j.do.symlink("/opt/jumpscale8/lib/JumpScale/","/usr/local/lib/python3.5/site-packages/JumpScale/")
+j.do.symlink("/opt/jumpscale8/lib/JumpScale/","/root/.ipython/JumpScale/")
+
+
+# import sys,base64
+# r=sys.stdin.read()
+# r=r.encode("ascii")
+# base64.b64encode(r)
+
+# from IPython import embed
+# embed()
 
 
 def sandbox1():
@@ -17,17 +26,23 @@ def sandbox1():
     excludeFileRegex=["/xml/","-tk/","/xml","/lib2to3"]
     excludeDirRegex=["/JumpScale","\.dist-info","config-x86_64-linux-gnu"]
 
-    dest = "/opt/jumpscale7/lib"
+    dest = "/opt/jumpscale8/lib"
 
     for path in paths:
         j.tools.sandboxer.copyTo(path,dest,excludeFileRegex=excludeFileRegex,excludeDirRegex=excludeDirRegex)
 
     try:
-        j.do.copyFile("/usr/bin/python","/opt/jumpscale7/bin/python")
+        j.do.copyFile("/usr/bin/python3.5","/opt/jumpscale8/bin/python")
     except Exception as e:
         print (e)
+
+    try:
+        j.do.copyFile("/usr/bin/python3.5","/opt/jumpscale8/bin/python3")
+    except Exception as e:
+        print (e)
+
         
-    j.tools.sandboxer.copyLibsTo(dest,"/opt/jumpscale7/bin/",recursive=True)
+    j.tools.sandboxer.copyLibsTo(dest,"/opt/jumpscale8/bin/",recursive=True)
     print ("SANDBOXING DONE")
 
 # sandbox1()
@@ -35,9 +50,14 @@ def sandbox1():
 
 
 d=j.atyourservice.debug.get("mytest")
-d.setHost("192.168.0.140")
-d.addPath("/opt/jumpscale7/")
-d.upload()
+d.setHost("192.168.0.105")
+d.setCache("192.168.0.140")
+# d.setNamespace("dedupe")
+d.addPath("/opt/jumpscale8/")
+
+
+d.upload(tocache=True,bootstrap=True)
+# d.installAYSFS()
 
 
 sys.exit()
